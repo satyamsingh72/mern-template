1ea63859f67895f99cd525a9b8ac509c0a9a4bd0
Satyam Singh <satyamsingh72@gmail.com>
2026-01-23 07:39:41 +0000

    Latest Code v2

--- server/src/constants/index.ts
+++ server/src/constants/index.ts
@@ -1,5 +1,5 @@
 const ORIGIN = '*'
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 5000
 
 // For "MongoDB Atlas": edit MONGO_URI in -> .env file
 // For "MongoDB Community Server": edit <DB_NAME> in -> MONGO_URI below
--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -8,13 +8,12 @@ import loginWithToken from '../controllers/auth/login-with-token'
 // initialize router
 const router = express.Router()
 
-// POST at route: http://localhost:8080/auth/register
+
 router.post('/register', [], register, errorHandler)
 
-// POST at path: http://localhost:8080/auth/login
 router.post('/login', [], login, errorHandler)
 
-// GET at path: http://localhost:8080/auth/account
+
 router.get('/login', [checkBearerToken], loginWithToken, errorHandler)
 
 export default router
