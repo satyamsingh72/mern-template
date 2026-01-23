2b0b25bbab7838232b266ca792ccb0f7c2a70b6b
Satyam Singh <satyamsingh72@gmail.com>
2026-01-23 09:15:52 +0000

    Latest Code v12

--- server/src/constants/index.ts
+++ server/src/constants/index.ts
@@ -2,11 +2,9 @@ const ORIGIN = '*'
 
 export const PORT = Number(process.env.PORT) || 5000;
 
-// For "MongoDB Atlas": edit MONGO_URI in -> .env file
-// For "MongoDB Community Server": edit <DB_NAME> in -> MONGO_URI below
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/<DB_NAME>'
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/mydb'
 const MONGO_OPTIONS = {}
 
 const JWT_SECRET = process.env.JWT_SECRET || 'unsafe_secret'
 
-export { ORIGIN, PORT, MONGO_URI, MONGO_OPTIONS, JWT_SECRET }
+export { ORIGIN, MONGO_URI, MONGO_OPTIONS, JWT_SECRET }
