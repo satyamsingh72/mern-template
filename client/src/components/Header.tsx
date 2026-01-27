cc7910396d88e9f9f6008f7083e8814ccc80e0d8
Satyam Singh <satyamsingh72@gmail.com>
2026-01-27 08:20:32 +0000

    vn17

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -36,7 +36,7 @@ const Header: React.FC<Props> = () => {
   return (
     <AppBar className='header' position='static'>
       <h1>React Authentication App</h1>
-      <br>
+      <br/>
       <h4>By --Satyam Singh Bhardwaj</h4>
 
       <IconButton onClick={openPopover}>
